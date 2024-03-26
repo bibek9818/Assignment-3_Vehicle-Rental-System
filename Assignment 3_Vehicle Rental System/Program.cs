f72639bcdd25f7b487ea24c196ffc4cb2b1c1824
Bibek <Bibek@DESKTOP-8JQ78AV>
2024-03-26 06:51:50 -0400

    Created methods in RentalAgency class

--- Assignment 3_Vehicle Rental System/Program.cs
+++ Assignment 3_Vehicle Rental System/Program.cs
@@ -7,14 +7,14 @@ class Program
 {
     static void Main(string[] args)
     {
- 
+        RentalAgency agency = new RentalAgency(10);
 
         Car car = new Car
         {
             Model = "Toyota Camry",
             Manufacturer = "Toyota",
             Year = 2022,
-            RentalPrice = 50,
+            RentalPrice = 220,
             Seats = 5,
             EngineType = "V6",
             Transmission = "Automatic",
@@ -26,7 +26,7 @@ class Program
             Model = "Ford F-150",
             Manufacturer = "Ford",
             Year = 2020,
-            RentalPrice = 80,
+            RentalPrice = 180,
             Capacity = 4,
             TruckType = "Pickup",
             FourWheelDrive = true
@@ -37,18 +37,30 @@ class Program
             Model = "Honda CBR500R",
             Manufacturer = "Honda",
             Year = 2019,
-            RentalPrice = 30,
+            RentalPrice = 330,
             EngineCapacity = 500,
             FuelType = "Gasoline",
             HasFairing = true
         };
 
-
+        agency.AddVehicle(car);
+        agency.AddVehicle(truck);
+        agency.AddVehicle(motorcycle);
 
         Console.WriteLine("Details of vehicles in fleet:");
-  
+        foreach (var vehicle in agency.GetFleet())
+        {
+            if (vehicle != null)
+            {
+                vehicle.DisplayDetails();
+            }
+        }
 
-       
+        agency.RentVehicle(car, 3);
+        agency.RentVehicle(truck, 5);
+        agency.RentVehicle(motorcycle, 2);
+
+        Console.WriteLine($"Total revenue: {agency.TotalRevenue}");
     }
 }
 
--- Assignment 3_Vehicle Rental System/RentalAgency.cs
+++ Assignment 3_Vehicle Rental System/RentalAgency.cs
@@ -11,7 +11,60 @@ namespace Assignment_3_Vehicle_Rental_System
 
     public class RentalAgency
     {
-    
+        private Vehicle[] Fleet { get; set; }
+        public double TotalRevenue { get; private set; }
+
+        public RentalAgency(int capacity)
+        {
+            Fleet = new Vehicle[capacity];
+        }
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            for (int i = 0; i < Fleet.Length; i++)
+            {
+                if (Fleet[i] == null)
+                {
+                    Fleet[i] = vehicle;
+                    return;
+                }
+            }
+            Console.WriteLine("Fleet is full, cannot add more vehicles.");
+        }
+
+        public void RemoveVehicle(Vehicle vehicle)
+        {
+            for (int i = 0; i < Fleet.Length; i++)
+            {
+                if (Fleet[i] == vehicle)
+                {
+                    Fleet[i] = null;
+                    return;
+                }
+            }
+            Console.WriteLine("Vehicle not found in the fleet.");
+        }
+
+        public void RentVehicle(Vehicle vehicle, int days)
+        {
+            if (Array.Exists(Fleet, v => v == vehicle))
+            {
+                double rentalCost = days * vehicle.RentalPrice;
+                TotalRevenue += rentalCost;
+                Console.WriteLine($"Vehicle rented for {days} days. Total cost: {rentalCost}");
+            }
+            else
+            {
+                Console.WriteLine("Vehicle not available for rent.");
+            }
+        }
+
+        public Vehicle[] GetFleet()
+        {
+            return Fleet;
+        }
+    }
+
 
 
 
